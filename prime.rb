c265ea5439349c266382bdb541cc18b3856ea38f
proactive-analyst-6157 <u.batsaikhan@gmail.com>
2020-04-14 22:10:03 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -3,6 +3,6 @@ def prime?(n)
   if n > 0
     (2..n-1).none? {|integer| n % integer == 0}
   else
-    (-2..n+1).none? {|integer| n % integer ==0}
+    (-2..n-1).none? {|integer| n % integer ==0}
   end
 end
